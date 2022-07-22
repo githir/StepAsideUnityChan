df011f12bde299382435b794676b1f79bd8e2f65
githir <githir@users.noreply.github.com>
2022-07-23 08:18:50 +0900

    1st try. use onBecameInvisible

--- Assets/InvisibleDestroyController.cs
+++ Assets/InvisibleDestroyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyController : MonoBehaviour
+public class InvisibleDestroyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
